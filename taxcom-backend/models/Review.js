172798646a21b35d4bb8336bd19f27d8a45ac12c
Moonshad Shahid <153177535+moonshad13@users.noreply.github.com>
2024-12-07 22:28:28 +0600

    backend removed again

--- taxcom-backend/models/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ReviewSchema = new mongoose.Schema({
-    author: { type: String, required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Review', ReviewSchema);
